01d8beca00e8a0f29b10be18df83b8b2ab6c25d3
bpleslie <bradleypleslie@gmail.com>
2016-12-10 13:27:58 -0800

    Create room scaffold

--- money-trees/db/schema.rb
+++ money-trees/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161210212624) do
+ActiveRecord::Schema.define(version: 20161210212730) do
 
   create_table "grows", force: :cascade do |t|
     t.string   "name"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20161210212624) do
     t.string   "name"
     t.integer  "strain_id"
     t.integer  "room_id"
+    t.integer  "grow_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
     t.integer  "grow_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
