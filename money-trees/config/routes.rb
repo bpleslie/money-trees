45186426135b06f5267751c89a03426a00ce30e9
bpleslie <bradleypleslie@gmail.com>
2016-12-10 16:03:24 -0800

    Add password and user auth, session

--- money-trees/config/routes.rb
+++ money-trees/config/routes.rb
@@ -1,21 +1,25 @@
 Rails.application.routes.draw do
-  root 'home#index'
-
-  get '/help', to: 'home#help'
-  get '/about', to: 'home#about'
-  get '/contact', to: 'home#contact'
-  get '/login', to: 'users#new'
-  get '/signup', to: 'users#new'
+  get 'sessions/new'
   get 'home/index'
   get 'home/contact'
   get 'home/about'
   get 'home/help'
+
+  get '/help', to: 'home#help'
+  get '/about', to: 'home#about'
+  get '/contact', to: 'home#contact'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/signup', to: 'users#new'
 
   resources :rooms
   resources :strains
   resources :plants
   resources :grows
   resources :users
+
+  root 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
