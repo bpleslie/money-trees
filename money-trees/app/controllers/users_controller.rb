c7b90d633cf3fbca77c40da97a762848402c6ac1
bpleslie <bradleypleslie@gmail.com>
2016-12-10 16:34:56 -0800

    Fix issues with logging in / out

--- money-trees/app/controllers/users_controller.rb
+++ money-trees/app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update]
 
   # GET /users
   # GET /users.json
@@ -70,6 +71,17 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email)
+      params.require(:user).permit(:name, :email, :password)
+    end
+
+    def logged_in_user
+      unless logged_in?
+        format.html { redirect_to login_path, notice: 'Please log in.' }
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
