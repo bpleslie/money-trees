6928a13d35a1ab34fab1f20024e95b6952c4c5f4
bpleslie <bradleypleslie@gmail.com>
2016-12-10 17:16:03 -0800

    Misc frontend cleanup, add model methods to get grow and strain name from plant

--- money-trees/app/models/plant.rb
+++ money-trees/app/models/plant.rb
@@ -2,4 +2,16 @@ class Plant < ApplicationRecord
   belongs_to :grow
   belongs_to :strain
   belongs_to :room
+
+  def strain
+    Strain.find(strain_id).name
+  end
+
+  def room
+    Room.find(room_id).name
+  end
+
+  def grow
+    Grow.find(grow_id).name.titleize
+  end
 end
